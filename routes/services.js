ce7106d10876c2bb2bfeb689bfd45a0b0522cb64
TenshiRachel <joshdebean@gmail.com>
2019-05-19 19:08:43 +0800

    Revert "Services route added"
    This reverts commit b2d0fad69b1a3fbc2e649d383e73cc071ef3c01f.

--- routes/services.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/viewServices', (req,res) =>{
-    res.render('./services/listServices')
-})
-
-module.exports = router;
