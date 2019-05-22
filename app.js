b810ccf888b13359f2e04aa636ceb5d93f04f714
Ryuse <34282683+Ryuse@users.noreply.github.com>
2019-05-22 15:15:20 +0800

    Revert "Add files via upload"
    This reverts commit 2a1d4fcd7be8d48314be97ca06a54d3d73077195.

--- app.js
+++ app.js
@@ -11,45 +11,10 @@ const mainRoute = require('./routes/main');
 const servicesRoute = require('./routes/services');
 const userRoute = require('./routes/user');
 
-
-//DB set up
-const outsourceDB = require('./config/DBConnection');
-outsourceDB.setUpDB(false);
-
-// Library to use MySQL to store session objects
-const MySQLStore = require('express-mysql-session');
-const db = require('./config/db'); // db.js config file
-
-
-
 const app = express();
 
 //==================================================================================================================================//
 //==================================================================================================================================//
-
-
-//Session Handling
-app.use(session({
-	key: 'outsource_session',
-	secret: 'tojiv',
-	store: new MySQLStore({
-		host: db.host,
-		port: 3306,
-		user: db.username,
-		password: db.password,
-		database: db.database,
-		clearExpired: true,
-		// How frequently expired sessions will be cleared; milliseconds:
-		checkExpirationInterval: 900000,
-		// The maximum age of a valid session; milliseconds:
-		expiration: 900000,
-	}),
-	resave: false,
-	saveUninitialized: false,
-}));
-
-
-
 
 //Handlebars
 app.engine('handlebars', exphbs({ 
@@ -57,11 +22,8 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-
-
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 
 
--- config/DBConfig.js
+++ config/DBConfig.js
@@ -3,8 +3,6 @@ const Sequelize = require('sequelize');
 // Bring in db.json which contains database name, username and password
 const db = require('./db');
 // Instantiates Sequelize with database parameters
-
-
 const sequelize = new Sequelize(db.database, db.username, db.password, {
     host: db.host, // Name or IP address of MySQL server
     dialect: 'mysql', // Tells sequelize that MySQL is used
@@ -12,8 +10,6 @@ const sequelize = new Sequelize(db.database, db.username, db.password, {
     define: {
         timestamps: false // Don't create timestamp fields in database
     },
-
-    
     pool: { // Database system params, don't need to know
         max: 5,
         min: 0,
