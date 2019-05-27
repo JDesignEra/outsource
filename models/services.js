08abdc8c0210fea6fa35bfc4ef2ccd960127559c
TenshiRachel <joshdebean@gmail.com>
2019-05-27 17:03:06 +0800

    Service model change & Search

--- models/services.js
+++ models/services.js
@@ -2,17 +2,35 @@ const Sequelize = require('sequelize');
 const db = require('../config/dbConfig');
 
 const Services = db.define('service', {
+    sid: {
+        type: Sequelize.INTEGER,
+        primaryKey = true,
+        allowNull = false
+    },
+    uid: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: user,
+            key: "uid"
+        }
+    },
     name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull = false
     },
     desc: {
         type: Sequelize.STRING(2000)
     },
     price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
+        allowNull = false
     },
     posterURL: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+    },
+    category: {
+        type: Sequelize.STRING,
+        allowNull = false
     }
 });
 
