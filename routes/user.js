064cfc33e3bf49d85d786e4072999e9235e5a1a6
Benedict <Blzh1206@gmail.com>
2019-05-27 16:59:50 +0800

    AAAAAAAAAA

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,12 @@ router.get('/profile', (req , res) => {
     res.render('user/profile')
 });
 
+router.get('/register',(req, res) => {
+    res.render('user/register')
+});
+
+router.get('/login',(req,res) => {
+    res.render('user/login')
+});
+
 module.exports = router;
