31b71ab4c1b2e353ce46ddef39d66f77d5467b9e
Ryuse <leemuel01234@gmail.com>
2019-05-29 16:25:24 +0800

    Stuff happened
    What was that with port 5000

--- models/users.js
+++ models/users.js
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream:models/users.js
+
 const Sequelize = require('sequelize');
 const db = require('../config/dbConfig');
 
@@ -55,62 +55,3 @@ const users = db.define('user', {
 });
 
 module.exports = users;
-=======
-const Sequelize = require('sequelize');
-const db = require('../config/dbConfig');
-
-
-const Users = db.define('user', {
-    uid:{
-        type: Sequelize.INTEGER,
-        allowNull:false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    username: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.TEXT,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    accType: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    name: {
-        type: Sequelize.STRING
-    },
-    paypal: {
-        type: Sequelize.STRING,
-    },
-    website: {
-        type: Sequelize.STRING
-    },
-    social_media: {
-        type: Sequelize.STRING
-    },
-    dob:{
-        type: Sequelize.DATE
-    },
-    gender: {
-        type: Sequelize.STRING
-    },
-    jobTitle: {
-        type: Sequelize.STRING
-    },  
-    skills:{
-        type: Sequelize.STRING
-    },
-    bio: {
-        type: Sequelize.STRING(5000)
-    }
-});
-
-module.exports = Users;
->>>>>>> Stashed changes:models/user.js
