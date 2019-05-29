2cf174f3322950d93816ac8b4a0c30f897a9908e
TenshiRachel <joshdebean@gmail.com>
2019-05-29 18:58:10 +0800

    Image upload + Fix

--- routes/services.js
+++ routes/services.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Services = require('../models/Services');
 const ensureAuthenticated = require('../middlewares/auth');
+const fs = require('fs');
+const upload = require('../helpers/imageUpload');
 
 router.get('/viewServices', (req, res)=>{
     res.render('./services/listServices');
@@ -114,6 +116,24 @@ router.get('/delete/:id', ensureAuthenticated, (req, res) => {
             })
         }
     })
+})
+
+router.post('/upload', ensureAuthenticated, (req, res) => {
+    // Creates user id directory for upload if not exist
+    if (!fs.existsSync('./public/uploads/' + req.user.id)) {
+        fs.mkdirSync('./public/uploads/' + req.user.id);
+    }
+    upload(req, res, (err) => {
+        if (err) {
+            res.json({ file: '/img/no-image.jpg', err: err });
+        } else {
+            if (req.file === undefined) {
+                res.json({ file: '/img/no-image.jpg', err: err });
+            } else {
+                res.json({ file: `/uploads/${req.user.id}/${req.file.filename}` });
+            }
+        }
+    });
 })
 
 module.exports = router;
