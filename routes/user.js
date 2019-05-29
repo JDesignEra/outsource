4824d6ff5afc96975049eb6e968c4293ddcf708d
Benedict <Blzh1206@gmail.com>
2019-05-29 16:11:23 +0800

    Register and Login
    Register Tabs and Login dropdown menu is done.

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+const alertMessage = require('../helpers/messenger');
+
+const passport = require('passport');
+
+const bcrypt = require('bcryptjs');
+
 router.get('/profile', (req , res) => {
 
     res.render('user/profile')
@@ -8,6 +15,64 @@ router.get('/profile', (req , res) => {
 
 router.get('/register',(req, res) => {
     res.render('user/register')
+});
+
+router.post('/register', (req, res) => {
+    let errors = [];
+    // Retrieves fields from register page from request body
+    let { name, email, password, password2 } = req.body;
+
+    // Checks if both passwords entered are the same
+    if (password != password2) {
+        errors.push({ text: 'Passwords do not match' });
+    }
+
+    // Checks that password length is more than 4
+    if (password.length < 4) {
+        errors.push({ text: 'Password must be at least 4 characters' });
+    }
+    if (errors.length > 0) {
+        res.render('user/register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+
+        });
+    }
+    else {
+        // If all is well, checks if user is already registered
+        User.findOne({ where: { email: req.body.email } })
+            .then(user => {
+                if (user) {
+                    // If user is found, that means email has already been
+                    // registered
+                    res.render('user/register', {
+                        error: user.email + ' already registered',
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    // Create new user record
+                    bcrypt.genSalt(10, function(err, salt) {
+                        bcrypt.hash(password, salt, function(err, hash) {
+                            // Store hash in your password DB.
+                            password = bcrypt.hashSync(password, salt)
+                            User.create({ name, email, password })
+                            .then(user => {
+                                alertMessage(res, 'success', user.name + ' added. Please login', 'fas fa-sign-in-alt', true);
+                                res.redirect('login');
+                            })
+                            .catch(err => console.log(err));
+                        });
+                        
+                    });
+                }
+            });
+    }
 });
 
 router.get('/login',(req,res) => {
