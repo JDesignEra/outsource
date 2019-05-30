63df0302e6bbad156ec8a3597f3dc7ea3b4743f7
Joel <tgm.joel@gmail.com>
2019-05-31 04:47:19 +0800

    dev 2.6.2
    * [ .gitignore ] added { \.__* }.
    * [ gulpfile.js ] added { gulp imgmin } to compress images.
    * [ gulpfile.js ] Changed { gulp } to scss to css compiler and image compress watcher.
    * [ gulpfile.js ] added { gulp scss2css } for manual scss to css compile.
    * ( public/img/* ) optimize images size.
    * ( views/files/view.handlebars ) WIP.
    * ( views/partials/files/_filesRow.handlebars ) WIP.
    
    - ( public/scss/bootstrap/.__* ) removed MAC OS generated file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+const imageMin = require('gulp-imagemin');
 
 function sass2css(done) {
     let x = gulp.src(['./public/scss/bootstrap/bootstrap.scss', './public/scss/mdb/mdb.scss'])
@@ -8,23 +9,37 @@ function sass2css(done) {
         .pipe(rename({extname: ".min.css"}))
         .pipe(gulp.dest('./public/css/vendor'));
 
-    x.on('data', function(chunk) {
+    output(x, 'sass2html');
+    done();
+}
+
+function imgMin(done) {
+    let x = gulp.src(['./public/img/**/*.png', './public/img/**/*.jpg', './public/img/**/*.jpeg', './public/img/**/*.gif', './public/img/**/*.svg'])
+        .pipe(imageMin())
+        .pipe(gulp.dest('./public/img/'));
+
+    output(x, 'imageMin');
+    done();
+}
+
+// For cmd/terminal output
+function output(gulpSrc, funcName) {
+    gulpSrc.on('data', function(chunk) {
         let contents = chunk.contents.toString().trim();
         let buffLength = process.stdout.columns;
         let hr = '\n\n' + Array(buffLength).join("_") + '\n\n';
 
         if (contents.length > 1) {
-            process.stdout.write('sass2html: ' + chunk.path + '\n');
+            process.stdout.write(funcName + ': ' + chunk.path + '\n');
         }
     });
 
     process.stdout.write('\n');
-    done();
 }
 
-function watch() {
+exports.scss2css = sass2css;
+exports.imgmin = imgMin;
+exports.default = function() {
     gulp.watch(['./public/scss/bootstrap/bootstrap.scss', './public/scss/mdb/mdb.scss'], sass2css);
-}
-
-exports.default = sass2css;
-exports.watch = watch;
+    gulp.watch('./public/img/**/*', imgMin);
+};
