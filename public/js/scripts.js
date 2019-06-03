5ac0cca19e1d41ad94d4e422ef014b60f2e8f678
TenshiRachel <joshdebean@gmail.com>
2019-06-03 21:19:23 +0800

    Filter fixing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,21 +1,21 @@
+// Animation End Fix
+let animationEnd = (function (el) {
+    let animations = {
+        "animation": "animationend",
+        "OAnimation": "oAnimationEnd",
+        "MozAnimation": "mozAnimationEnd",
+        "WebkitAnimation": "webkitAnimationEnd"
+    };
+
+    for (var t in animations) {
+        if (el.style[t] !== undefined) {
+            return animations[t];
+        }
+    }
+})(document.createElement("fakeelement"));
+
 $(document).ready(function () {
     // Joel
-    // Animation End Fix
-    let animationEnd = (function (el) {
-        let animations = {
-            "animation": "animationend",
-            "OAnimation": "oAnimationEnd",
-            "MozAnimation": "mozAnimationEnd",
-            "WebkitAnimation": "webkitAnimationEnd"
-        };
-
-        for (var t in animations) {
-            if (el.style[t] !== undefined) {
-                return animations[t];
-            }
-        }
-    })(document.createElement("fakeelement"));
-
     // ToTop Button
     $(window).scroll(function () {
         let focus = $('#toTopAction');
@@ -36,77 +36,60 @@ $(document).ready(function () {
     });
 
     // File(s) Upload
-    let focuses = [];
+    $(function () {
+        let focuses = [];
 
-    if ($('form.files-upload')[0] !== undefined) {
-        focuses = focuses.concat($('form.files-upload'));
-    }
+        if ($('form.files-upload')[0] !== undefined) {
+            focuses = focuses.concat($('form.files-upload').toArray());
+        }
 
-    if ($('form.file-upload')[0] !== undefined) {
-        focuses = focuses.concat($('form.file-upload'));
-    }
+        if ($('form.file-upload')[0] !== undefined) {
+            focuses = focuses.concat($('form.file-upload').toArray());
+        }
 
-    focuses.forEach(el => {
-        // Populate Addition DOM Elements
-        el.prepend(
-            '<div class="wrapper card card-body primary-gradient view overlay z-depth-2">' +
-                '<div class="mask rgba-black-slight" style=""></div>' +
-                '<div class="card-text">' +
-                    '<i class="fas fa-cloud-upload-alt fa-5x"></i>' +
-                    '<p class="font-weight-bolder">Drag and drop a file here or click</p>' +
-                '</div>' +
-            '</div>'
-        );
+        for (let i = 0; i < focuses.length; i++) {
+            let input = $(focuses[i]).find(['input[type=file]']);
+            let feedback = $(focuses[i]).find('.feedback');
 
-        el.append('<div class="feedback small text-danger text-center"</div>');
+            $(document).on({
+                'dragover': function (e) {
+                    e.preventDefault();
+                },
+                'dragleave': function (e) {
+                    e.preventDefault();
+                },
+                'drop': function (e) {
+                    e.preventDefault();
+                    
+                    let uploads = e.originalEvent.dataTransfer.files || (e.dataTransfer && e.dataTransfer.files) || e.target.files;
+                    let error;
+                    feedback.addClass('d-none');
 
-        // Attach EvenListener
-        let input = el.children('input[type=file]');
-        let feedback = el.children('.feedback');
+                    if (!$(focuses[i]).hasClass('file-upload') && uploads.length > 1) {
+                        error = 'Only 1 file upload is allowed';
+                    }
+                    else {
+                        for (let x = 0; x < uploads.length; x++) {
+                            if (uploads[i].size > 5368709120) {
+                                error = 'File size cannot exceed 5GB.';
+                            }
 
-        $(el).on({
-            'dragover': function (e) {
-                e.preventDefault();
-            },
-            'dragleave': function (e) {
-                e.preventDefault();
-            },
-            'drop': function (e) {
-                e.preventDefault();
-
-                let uploads = e.originalEvent.dataTransfer.files || (e.dataTransfer && e.dataTransfer.files) || e.target.files;
-                let error;
-                feedback.addClass('d-none');
-
-                if (!$(el).hasClass('file-upload') && uploads.length > 1) {
-                    error = 'Only 1 file upload is allowed';
-                }
-                else {
-                    for (let i = 0; x < uploads.length; i++) {
-                        if (uploads[i].size > 5368709120) {
-                            error = 'File size cannot exceed 5GB.';
-                        }
-
-                        if (error !== undefined) {
-                            $(input).prop('files', uploads);
-                            console.log($(input).attr('files'));
+                            if (error !== undefined) {
+                                $(input).prop('files', uploads);
+                                console.log($(input).attr('files'));
+                            }
                         }
                     }
-                }
-            },
-            'click': function (e) {
-                $(input).trigger('click');
-            },
-        });
+                },
+                'onclick': function() {
+                    $(input).click();
+                },
+            }, focuses[i]);
 
-        $(input).on('change', function () {
-            $(el).submit();
-        });
-
-        // Prevent Bubbling Event
-        $(input).on('click', function (e) {
-            e.stopPropagation();
-        });
+            $(document).on('change', input, function() {
+                $(focus[i]).submit();
+            });
+        }
     });
 
     // Tooltips
@@ -130,26 +113,26 @@ $(document).ready(function () {
         });
     });
 
-    // $('#posterUpload').on('change', function () {
-    //     let image = $("#posterUpload")[0].files[0];
-    //     let formdata = new FormData();
-    //     formdata.append('posterUpload', image);
-    //     $.ajax({
-    //         url: '/video/upload',
-    //         type: 'POST',
-    //         data: formdata,
-    //         contentType: false,
-    //         processData: false,
-    //         'success': (data) => {
-    //             $('#poster').attr('src', data.file);
-    //             $('#posterURL').attr('value', data.file);// sets posterURL hidden field
-    //             if (data.err) {
-    //                 $('#posterErr').show();
-    //                 $('#posterErr').text(data.err.message);
-    //             } else {
-    //                 $('#posterErr').hide();
-    //             }
-    //         }
-    //     });
-    // });
+    $('#posterUpload').on('change', function () {
+        let image = $("#posterUpload")[0].files[0];
+        let formdata = new FormData();
+        formdata.append('posterUpload', image);
+        $.ajax({
+            url: '/video/upload',
+            type: 'POST',
+            data: formdata,
+            contentType: false,
+            processData: false,
+            'success': (data) => {
+                $('#poster').attr('src', data.file);
+                $('#posterURL').attr('value', data.file);// sets posterURL hidden field
+                if (data.err) {
+                    $('#posterErr').show();
+                    $('#posterErr').text(data.err.message);
+                } else {
+                    $('#posterErr').hide();
+                }
+            }
+        });
+    });
 });
--- public/js/servicesscript.js
+++ public/js/servicesscript.js
@@ -1,11 +1,11 @@
 
 function search() {
-    var input = document.getElementById('search').value;
-    var filter = input.toUpperCase();
-    var cards = document.querySelectorAll('.servicecards');
-    var name = document.querySelectorAll('.name');
-    for (var i = 0; i < cards.length; i++) {
-        var compare = name[i].dataset.names;
+    let input = document.getElementById('search').value;
+    let filter = input.toUpperCase();
+    let cards = document.querySelectorAll('.servicecards');
+    let name = document.querySelectorAll('.name');
+    for (let i = 0; i < cards.length; i++) {
+        let compare = name[i].dataset.names;
         if (compare.toUpperCase().includes(filter)) {
             cards[i].style.display = "";
         }
@@ -17,14 +17,49 @@ function search() {
 
 document.getElementById('searchbut').addEventListener('click', search);
 
-function filterSelection(category){
-    var cards = document.querySelectorAll('.servicecards');
-    for (var i = 0; i < cards.length; i++) {
-        if (cards[i].classList.contains(category) || category === "all") {
-            cards[i].style.display = "";
+$('select.category-select').on('change', function(e) {
+    let cards = $('.servicecards');
+    let category = $(this).find('option:selected').text().replace(/\s/g, '');
+    
+    for (let i = 0; i < cards.length; i++) {
+        let focus = cards[i];
+
+        if (($(focus).hasClass(category) || category === "ShowAll")) {
+            if ($(focus).hasClass('d-none')) {
+                $(focus).removeClass('d-none');
+                console.log('WHY')
+                $(focus).addClass('animated fadeIn').on(animationEnd, function() {
+                    $(this).removeClass('animated fadeIn');
+                });
+            }
         }
         else {
-            cards[i].style.display = "none";
+            console.log('What')
+            if (!$(focus).hasClass('d-none')){
+                $(focus).addClass('animated fadeOut').on(animationEnd, function(){
+                    $(this).removeClass('animated fadeOut');
+                    $(focus).addClass('d-none');
+                })
+            }
+            
+            // if (!$(focus).hasClass('d-none')) {
+            //     $(focus).addClass('animated fadeOut').on(animationEnd, function() {
+                    
+            //         $(this).removeClass('animated fadeOut');
+            //     });
+            // }
         }
     }
-}
+});
+
+// function filterSelection(category){
+//     let cards = document.querySelectorAll('.servicecards');
+//     for (let i = 0; i < cards.length; i++) {
+//         if (cards[i].classList.contains(category) || category === "all") {
+//             cards[i].style.display = "";
+//         }
+//         else {
+//             cards[i].style.display = "none";
+//         }
+//     }
+// }
