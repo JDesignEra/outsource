6834242033f68a8de2b91f527299921558a9a3c8
TenshiRachel <joshdebean@gmail.com>
2019-06-07 22:26:05 +0800

    Revert "Added client side validation for categories"

--- public/js/servicesscript.js
+++ public/js/servicesscript.js
@@ -56,27 +56,6 @@ $('select.category-select').on('change', function(e) {
     }
 });
 
-function categoryCheck() {
-    var categories = document.getElementsByName('categories');
-    var error = document.getElementById('categoryErr');
-    var button = document.getElementById('butaddService');
-    var ticks = 0;
-    console.log('FIRE')
-    for (var i = 0; i < categories.length; i++) {
-        if (categories[i].checked == true) {
-            ticks++;
-        }
-    }
-    if (ticks == 0) {
-        error.style.display = 'block';
-        button.disabled = true;
-    }
-    else {
-        error.style.display = 'none';
-        button.disabled = false;
-    }
-}
-
 // function filterSelection(category){
 //     let cards = document.querySelectorAll('.servicecards');
 //     for (let i = 0; i < cards.length; i++) {
