a0e46b5564a794bb324d05cad447649cf83eb206
Ryuse <leemuel01234@gmail.com>
2019-06-27 14:55:13 +0800

    Profile routes

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const { isAuth } = require('../middlewares/auth');
 const upload = multer({dest: './public/uploads/temp'});
 const profileController = require('../controllers/profile');
 
 // ToDo: /uid url
-router.get('/', profileController.index);
-router.get('/submit', profileController.submit)
+router.get('/', isAuth, profileController.index);
+router.get('/edit', isAuth, profileController.editProfile);
+router.get('/submit', isAuth, profileController.submit)
 router.post('/submit', upload.single('coverPicture'), profileController.submitProject)
+router.get('/delete/:id'), profileController.deleteProject)
 module.exports = router;
