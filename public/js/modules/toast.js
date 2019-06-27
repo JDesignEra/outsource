d9399033add208a2e9cdab971ebbb597a296277d
Joel <tgm.joel@gmail.com>
2019-06-27 17:21:32 +0800

    dev 5.7.1
    * ( public/js/modules/toast.js ) bug fixed.
    * ( public/js/modules/validation.js ) added compatibility with input-group md-outline.

--- public/js/modules/toast.js
+++ public/js/modules/toast.js
@@ -1,8 +1,7 @@
 // toast by Joel
 /**
  * * Takes in toastMsgs variable from base.handlebars.
- * * toastMsgs variable gets dictionary object,
- * * it can be found it app.js ( Render Engine Global Variable ).
+ * * toastMsgs variable is a dictionary object.
 */
 let toast = (function() {
     'use strict';
@@ -12,12 +11,18 @@ let toast = (function() {
         if (typeof toastMsgs !== 'undefined') {
             toastr.options = {
                 'closeButton': true,
-                'progressBar': true
+                'progressBar': true,
+                'newestOnTop': true,
+                'extendedTimeOut': 0,
+                'hideDuration': 300,
+                'timeOut': 2000,
+                'extendedTimeOut': 1000,
             }
 
             Object.keys(toastMsgs).forEach(function(key) {
+                _this = toastMsgs[key];
+
                 if (['info', 'warning', 'success', 'error'].includes(key)) {
-                    let _this = toastMsgs[key];
                     for (let i = 0, n = _this.length; i < n; i++) {
                         toastr[key](_this[i]);
                     }
