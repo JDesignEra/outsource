80e07100462b937540f1c68477c899e72352757f
Layzee <layzeeisme@gmail.com>
2019-07-04 12:11:33 +0800

    Changed Job Variable

--- models/jobs.js
+++ models/jobs.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../config/dbConfig');
 const users = require('./users');
 
-const services = db.define('job', {
+const job = db.define('job', {
     id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
