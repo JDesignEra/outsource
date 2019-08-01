a26102bf62a95db293dff6c059222b0e7c776b26
Benedict <Blzh1206@gmail.com>
2019-08-01 15:19:52 +0800

    Facebook API

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
     'facebookAuth' : {
-        'clientID': '',
-        'clientSecret': '',
+        'clientID': '900093130361854',
+        'clientSecret': 'ce031e037bf20d523f2c62f3ae0da1f0',
         'callbackURL': 'http://localhost:5000/auth/facebook/callback'
     }
 }
