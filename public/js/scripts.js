8f9104beeef1be7e78d97a6173eb27d0255b66ee
Joel <joel@jdesignera.com>
2019-08-01 15:33:10 +0800

    dev 15.0.1
    * ( public/js/scripts.js ) fixed fb button issue.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -132,40 +132,38 @@ $(function () {
 });
 
 // Benedict
-$(function() {
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '900093130361854',
-            cookie     : true,
-            xfbml      : true,
-            version    : 'v4.0'
-        });
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '900093130361854',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v4.0'
+    });
 
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
-    };
-    
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    
-    function statusChangeCallback(response){
-        if(response.status === 'connected'){
-            console.log('Logged in and authenticated');
-        }
-        else{
-            console.log('Not Authenticated');
-        }
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response){
+    if(response.status === 'connected'){
+        console.log('Logged in and authenticated');
     }
-    
-    function checkLoginState() {
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
+    else{
+        console.log('Not Authenticated');
     }
-});
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
