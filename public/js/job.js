4c243a75f6508d7bf544e470f0cefa08456bb06c
TenshiRachel <joshdebean@gmail.com>
2019-08-02 02:34:33 +0800

    Consistent Job dashboard

--- public/js/job.js
+++ public/js/job.js
@@ -1,17 +1,78 @@
 // Material Design example
 $(document).ready(function () {
-    $('#dtMaterialDesignExample').DataTable();
-    $('#dtMaterialDesignExample_wrapper').find('label').each(function () {
-        $(this).parent().append($(this).children());
+    let table = $('#jobs-table', 'section.jobs');
+    let trs = $('tbody tr', '#jobs-table');
+
+    table.DataTable({
+        ordering: true,
+        order: [[1, 'asc']],
+        paging: false,
+        info: false,
+        searching: false,
+        language: { emptyTable: 'No requested service.' },
+        columnDefs: [{
+            orderable: false,
+            targets: 5
+        }],
     });
-    $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('input').each(function () {
-        $('input').attr("placeholder", "Search");
-        $('input').removeClass('form-control-sm');
+
+    $('input[name="search"]', 'section.jobs').on('keyup', function() {
+        let val = $(this).val().toLowerCase();
+        let shown = trs.length;
+
+        trs.each(function() {
+            let _this = $(this);
+
+            if (val) {
+                let tds = _this.children('td[headers]:not([headers="action"], [id="empty-search"])');
+                let show = false;
+
+                for (let i = 0, n = tds.length; i < n && !show; i++) {
+                    td = $(tds[i]);
+                    
+                    if (td.text().toLowerCase().indexOf(val) > -1) {
+                        show = true;
+                    }
+                }
+                
+                if (show && _this.attr('style')) {
+                    _this.removeAttr('style');
+
+                    _this.addClass('flipInX').one(animationEnd, function() {
+                        _this.removeClass('flipInX');
+                    });
+                }
+                else if (!show) {
+                    if (!_this.attr('style')) {
+                        _this.addClass('flipOutX').one(animationEnd, function() {
+                            _this.removeClass('flipOutX');
+                            _this.prop('style', 'display: none !important;');
+                        });
+                    }
+
+                    shown--;
+                }
+            }
+            else {
+                if (_this.attr('style')) {
+                    _this.removeAttr('style');
+                    _this.addClass('flipInX').one(animationEnd, function() {
+                        $(this).removeClass('flipInX');
+                    });
+                }
+            }
+        });
+
+        $('#empty-search', '#jobs-table').remove();
+        
+        if (shown < 1) {
+            $('tbody', '#jobs-table').prepend(`
+                <tr id="empty-search">
+                    <td class="rounded-bottom" colspan=100>
+                        Couldn\'t find anything for <span class="font-weight-bolder">${val}</span>
+                    </td>
+                </tr>
+            `);
+        }
     });
-    $('#dtMaterialDesignExample_wrapper .dataTables_length').addClass('d-flex flex-row');
-    $('#dtMaterialDesignExample_wrapper .dataTables_filter').addClass('md-form');
-    $('#dtMaterialDesignExample_wrapper select').removeClass('custom-select custom-select-sm form-control form-control-sm');
-    $('#dtMaterialDesignExample_wrapper select').addClass('mdb-select');
-    $('#dtMaterialDesignExample_wrapper .mdb-select').materialSelect();
-    $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('label').remove();
 });
