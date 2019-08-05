9ad0c780bf32af7621bea6e11f14f1966bda01c4
Ryuse <leemuel01234@gmail.com>
2019-08-05 10:48:10 +0800

    Display Portfolio Changes

--- public/js/displayPortfolio.js
+++
@@ -1,8 +0,0 @@
-$(window).ready(function () {
-    container = document.querySelectorAll("div #contentTest")
-    content = document.querySelectorAll("div #realcontent")
-    for(i = 0; i < container.length; i++){
-        container[i].innerHTML = content[i].innerHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
-     
-    }
-});
