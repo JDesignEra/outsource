52f8484bd1ca3afabcf7fefe674567879520f673
Ryuse <leemuel01234@gmail.com>
2019-08-05 11:11:12 +0800

    Just added isAuths

--- routes/profile.js
+++ routes/profile.js
@@ -7,22 +7,22 @@ const authController = require('../controllers/auth');
 const profileController = require('../controllers/profile');
 
 //Notifications
-router.get('/notifications/', profileController.viewNotification)
-router.get('/deleteNotifications/:id', profileController.deleteNotification)
+router.get('/notifications/', isAuth, profileController.viewNotification)
+router.get('/deleteNotifications/:id', isAuth, profileController.deleteNotification)
 router.get('/deleteAllNotifications/:category', profileController.deleteAllNotification)
 
 //Profile
 router.get('/', isAuth, profileController.index);
-router.get('/openProject/:open', profileController.index)
+router.get('/openProject/:open', isAuth, profileController.index)
 
 router.get('/view/:id', profileController.viewProfile);
 router.get('/view/:id/:open', profileController.viewProfile)
-router.get('/follow/:id', profileController.follow)
-router.get('/unfollow/:id', profileController.unfollow)
+router.get('/follow/:id', isAuth, profileController.follow)
+router.get('/unfollow/:id', isAuth, profileController.unfollow)
 
 //Like Project
-router.get('/like/:id', profileController.likeProject)
-router.get('/unlike/:id', profileController.unlikeProject)
+router.get('/like/:id', isAuth, profileController.likeProject)
+router.get('/unlike/:id', isAuth, profileController.unlikeProject)
 router.get('/viewProject/:id', profileController.viewProjectUpdate)
 
 //Edit Profile
@@ -30,22 +30,22 @@ router.get('/edit', isAuth, profileController.editProfile);
 router.post('/edit', isAuth, profileController.editProfilePost);
 
 //Comment Project
-router.post('/commentProject/:projectID', profileController.postComment)
+router.post('/commentProject/:projectID', isAuth, profileController.postComment)
 
 //Submit Project
 router.get('/submit', isAuth, profileController.submit)
-router.post('/submit', upload.single('coverPicture'), profileController.submitProject)
+router.post('/submit', isAuth, upload.single('coverPicture'), profileController.submitProject)
 
 //Edit Project
 router.get('/editProject/:id', isAuth, profileController.editProject)
 router.post('/editProject/:id', upload.single('coverPicture'), isAuth, profileController.editProjectPost)
 
 //View and delete Project
-router.get('/delete/:id', profileController.deleteProject)
+router.get('/delete/:id', isAuth, profileController.deleteProject)
 
 //Change and Delete Account
-router.get('/edit/:id', authController.delete);
-router.get('/auth/reset', authController.changepw);
+router.get('/edit/:id', isAuth, authController.delete);
+router.get('/auth/reset', isAuth, authController.changepw);
 
 
 module.exports = router;
--- routes/services.js
+++ routes/services.js
@@ -13,15 +13,15 @@ router.get('/manage', isAuth, servicesController.management)
 
 router.get('/add', isAuth, servicesController.add)
 
-router.post('/add', upload.single('serviceposter'), servicesController.addpost)
+router.post('/add', isAuth, upload.single('serviceposter'), servicesController.addpost)
 
 router.get('/edit/:id', isAuth, servicesController.edit)
 
-router.post('/edit/:id', upload.single('serviceposter'), servicesController.save)
+router.post('/edit/:id', isAuth, upload.single('serviceposter'), servicesController.save)
 
 router.get('/delete/:id', isAuth, servicesController.delete)
 
-router.post('/fav/:id', servicesController.fav)
+router.post('/fav/:id', isAuth, servicesController.fav)
 
 router.get('/requests', isAuth, servicesController.requests)
 
