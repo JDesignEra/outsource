fc060c06f82c574eacd4029923472d4392bc7a55
Joel <joel@jdesignera.com>
2019-08-06 14:06:21 +0800

    18.6.3
    * ( controllers/files.js ) fixed card height for both local and server host.

--- public/js/index.js
+++ public/js/index.js
@@ -1,74 +1,68 @@
 // index by Joel
-$(function() {
-    let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    let observer = new MutationObserver(function(mutations, observer) {
-        mutations.forEach(obj => {
-            console.log(obj);
+let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-            let value = obj.target.attributes[1].nodeValue;
+let observer = new MutationObserver(function(mutations) {
+    let wrappers = $('.card-wrapper', 'section.home');
 
-            if (parseFloat(value.slice(7, -3)) > 0) {
-                obj.target.childNodes[1].childNodes[1].classList.add('h-100');
-                obj.target.childNodes[1].childNodes[3].classList.add('h-100');
-            }
-        });
-    });
+    mutations.forEach((obj, i) => {
+        let height = obj.target.attributes[1].nodeValue;
+        height = parseFloat(height.slice(7, -3));
 
-    let wrapper = document.querySelectorAll('section.home .card-wrapper');
+        if  (height > 0) {
+            let wrapper = $(wrappers[i]);
+            let faces = wrapper.find('.face');
 
-    wrapper.forEach(el => {
-        observer.observe(el, {
-            subtree: false,
-            attributes: true,
-            attributeFilter: ['style']
-        });
-    });
+            faces.each(function() {
+                let _this = $(this);
+                let imgTop = _this.find('.card-img-top');
 
-    let cardWrapper = $('.card-wrapper', 'section.home');
-    let cardFaces = cardWrapper.find('.face');
-
-    cardWrapper = $('.card-wrapper', 'section.portfolio-views, section.random-portfolios');
-
-    cardWrapper.each(function() {
-        let wrapper = $(this);
-        let cardFaces = cardWrapper.find('.face');
-
-        cardFaces.each(function() {
-            let _this = $(this);
-            let height = parseFloat(wrapper.css('height'));
-            let overlayHeight = _this.find('.view.overlay').height();
-            let imgTop = _this.find('.card-img-top');
-            let imgHeight = imgTop.height();
-
-            if (!_this.hasClass('h-100')) {
                 _this.addClass('h-100');
-            };
 
-            if (imgHeight > overlayHeight) {
-                let calcMargin = Math.abs((imgHeight - overlayHeight) / 2);
-                imgTop.css('margin-top', `${-calcMargin}px`);
-            }
-            
-            if (height - imgHeight < 300) {
-                _this.find('.card-body').css('max-height', `300px`);
-            }
-            else {
-                _this.find('.card-body').css('max-height', `${height - imgHeight}px`);
-            }
-
-            if (_this.hasClass('front')) {
-                let content = _this.find('.flex-fill.overflow-auto.card-text');
-                let contentImg = content.find('img');
-                
-                contentImg.each(function() {
+                imgTop.each(function(el) {
                     let _this = $(this);
-                    let align = _this.parent().css('text-align');
+                    let overlay = $(this).parent('.view.overlay');
 
-                    if (align === 'center') {
-                        _this.addClass('mx-auto');
+                    let overlayHeight = overlay.height();
+                    let imgHeight = _this.outerHeight();
+
+                    if (imgHeight > overlayHeight) {
+                        let calcMargin = Math.abs((imgHeight - overlayHeight) / 2);
+    
+                        imgTop.css('margin-top', `-${calcMargin}px`);
+                    }
+
+                    if (height - imgHeight < 300) {
+                        _this.parent().parent('face').find('.card-body').css('max-height', `300px`);
+                    }
+                    else {
+                        _this.parent().parent('face').css('max-height', `${height - imgHeight}px`);
                     }
                 });
-            }
-        });
+
+                if (_this.hasClass('front')) {
+                    let content = _this.find('.flex-fill.overflow-auto.card-text');
+                    let contentImg = content.find('img');
+                    
+                    contentImg.each(function() {
+                        let _this = $(this);
+                        let align = _this.parent().css('text-align');
+    
+                        if (align === 'center') {
+                            _this.addClass('mx-auto');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+let wrapper = document.querySelectorAll('section.home .card-wrapper');
+
+wrapper.forEach(el => {
+    observer.observe(el, {
+        subtree: false,
+        attributes: true,
+        attributeFilter: ['style']
     });
 });
